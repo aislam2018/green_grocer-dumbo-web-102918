86a17b53ffc16739d5f35d3c61bb8a230d93822d
anik1606 <anik1606@gmail.com>
2018-10-24 01:13:36 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -42,12 +42,13 @@ def apply_clearance(cart)
 end
 
 def checkout(cart, coupons)
-  total = 0 
   consolidated_cart = consolidate_cart(cart)
   couponed_cart = apply_coupons(consolidated_cart, coupons)
-  discount_cart = apply_clearance(couponed_cart)
-  discounted_cart.each do |item, features|
-    total += features[:price] * features[:count]
-   end 
-     total = total * 0.90 if total > 100
+  final_cart = apply_clearance(couponed_cart)
+  total = 0
+  final_cart.each do |name, properties|
+    total += properties[:price] * properties[:count]
+  end
+  total = total * 0.9 if total > 100
+  total
 end
