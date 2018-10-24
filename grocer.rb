7c52a7554d6a98a78974b304e6e89750591ae51c
anik1606 <anik1606@gmail.com>
2018-10-24 00:46:28 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -1,9 +1,33 @@
+require 'pry'
 def consolidate_cart(cart)
-  # code here
+  result = {}
+  cart.each do |item_hash|
+    item_hash.each do |item, features|
+      if result[item]
+        features[:count] += 1 
+      else
+        features[:count] = 1 
+        result[item] = features
+      end
+    end 
+  end
+    result
 end
 
 def apply_coupons(cart, coupons)
-  # code here
+  coupons.each do |coupon|
+    name = coupon[:item]
+    if cart[name] && cart[name][:count] >= coupon[:num]
+      if cart["#{name} W/COUPON"]
+        cart["#{name} W/COUPON"][:count] += 1 
+      else
+        cart["#{name} W/COUPON"] = {:count => 1 , :prince => coupon[:cost]
+        cart["#{name} W/COUPON"][:clearance] = cart[name][:clearance]
+      end
+      cart[name][:count] -= coupon[:num] 
+    end
+  end 
+  cart
 end
 
 def apply_clearance(cart)
